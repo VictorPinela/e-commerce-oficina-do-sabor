ac0f2be62ff0afab47041897d14737c068f7c89f
victor.pinela@fatec.sp.gov.br <61033309+VictorPinela@users.noreply.github.com>
2020-10-07 21:36:43 -0300

    atualização tabela cesta e Programação botões cesta

--- src/Components/TabCesta/index.js
+++ src/Components/TabCesta/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import styles from './styles.css'
+import TabItem from './TabItem.js'
+import TabTotal from './TabTotal.js'
 
-const TabCesta = () => {
-    return(
-        <div id='teste'>teste</div>
+function TabCesta() {
+    return (
+        <div>
+            <table id='tabela'>
+                <tr id='tr'>
+                    <td id='td'>Item</td>
+                    <td id='td'>Qtd.</td>
+                    <td id='td'>Valor</td>
+                </tr>
+            </table>
+            <span id='tab1'>
+            <TabItem />
+            <TabItem />
+            </span>
+            <TabTotal />
+        </div>
     );
 }
 
--- src/Paginas/Cesta-Produto/index.js
+++ src/Paginas/Cesta-Produto/index.js
@@ -2,46 +2,41 @@ import React from 'react';
 import styles from './styles.css'
 import Menu from '../../Components/Menu';
 import TabCesta from '../../Components/TabCesta';
+import TabItem from '../../Components/TabCesta/TabItem';
 
 
 const Cesta = () => {
     return (
-            <div id='areaCesta'>
-                <div id='bodyCesta'>
-                    <Menu />
-                    <div className='form-Cesta'>
-                        <div className='Cesta'>
-                            <div className='borda-interna'>
-                                <h1 id='tituloPag'>Sua cesta</h1>
-                                <TabCesta />
-                                <table id='tabela'>
-                                    <tr>
-                                        <td>Item</td>
-                                        <td>Quantidade</td>
-                                        <td>Valor</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Pizza</td>
-                                        <td>1</td>
-                                        <td>R$30,00</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Total</td>
-                                        <td>1</td>
-                                        <td>R$30,00</td>
-                                    </tr>
-                                </table>
-                                <div id='btn'>
-                                    <button id='botao'>Limpar cesta</button>
-                                    <button id='botao'>Finalizar Compra</button>
-                                </div>
+        <div id='areaCesta'>
+            <div id='bodyCesta'>
+                <Menu />
+                <div className='form-Cesta'>
+                    <div className='Cesta'>
+                        <div className='borda-interna'>
+                            <h1 id='tituloPag'>Sua cesta</h1>
+                            <TabCesta />
+                            <div id='btn'>
+                                <button id='botao1' onClick={()=> Deletar()}>Limpar cesta</button>
+                                <button id='botao2' onClick={()=> Finalizar()}>Finalizar Compra</button>
                             </div>
                         </div>
                     </div>
-
                 </div>
             </div>
+        </div>
     );
+}
+
+function Deletar(){
+    if(document.getElementById("tab1") != null){
+    document.getElementById("tab1").remove();}
+    else(
+        alert('Cesta vazia!')
+    )
+}
+
+function Finalizar(){
+    alert('Compra finalizada!')
 }
 
 export default Cesta;
