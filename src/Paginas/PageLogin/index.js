6b32d8918dad3c3b17e297e6d246e9547dc1cd53
Caro-line-lima <carolinesilvalima@outlook.com>
2020-10-08 21:32:35 -0300

    PageLogin alterada

--- src/Paginas/PageLogin/index.js
+++ src/Paginas/PageLogin/index.js
@@ -7,8 +7,6 @@ import Menu from '../../Components/Menu';
 import Footer from '../../Components/Footer';
 
 
-
-
 const PageLogin = props => {
 
     return (
@@ -24,13 +22,13 @@ const PageLogin = props => {
                                     <h1 id='tituloPag'>Login</h1>
                                     <form method='post' action=''> 
                                          <p> 
-                                            <label for='nome-login'>Seu nome</label>
-                                            <input id='nome-login' name='nome_login' required='required' type='text' placeholder='ex. contato@htmlecsspro.com'/>
+                                            <label for='nome-login'>E-mail</label>
+                                            <input id='nome-login' name='nome_login' required='required' type='email' placeholder='Ex.: seuemail@email.com.br'/>
                                         </p>
                                         
                                         <p> 
-                                            <label for='email-login'>Seu e-mail</label>
-                                            <input id='email-login' name='email_login' required='required' type='password' placeholder='ex. senha' /> 
+                                            <label for='email-login'>Senha</label>
+                                            <input id='email-login' name='email_login' required='required' type='password' placeholder='Insira sua senha' /> 
                                         </p>
                                                                         
                                         <p> 
@@ -40,7 +38,7 @@ const PageLogin = props => {
                                         <p className='link'>
                                             Ainda não tem conta?
                                             <Link to='#paracadastro'>
-                                                Cadastre-se 
+                                                 Cadastre-se 
                                             </Link>
                                         </p>
 
@@ -51,18 +49,42 @@ const PageLogin = props => {
                                 <h1 id='tituloPag'>Cadastro</h1>
                                     <form method='post' action=''>                                     
                                         <p> 
-                                            <label for='nome_cad'>Seu nome</label>
-                                            <input id='nome_cad' name='nome_cad' required='required' type='text' placeholder='nome' />
+                                            <label for='nome_cad'>Seu nome completo</label>
+                                            <input id='nome_cad' name='nome_cad' required='required' type='text' placeholder='Nome Completo' />
                                         </p>
                                         
                                         <p> 
                                             <label for='email_cad'>Seu e-mail</label>
-                                            <input id='email_cad' name='email_cad' required='required' type='email' placeholder='contato@htmlecsspro.com'/> 
+                                            <input id='email_cad' name='email_cad' required='required' type='email' placeholder='seuemail@email.com.br'/> 
                                         </p>
                                         
                                         <p> 
                                             <label for='senha_cad'>Sua senha</label>
-                                            <input id='senha_cad' name='senha_cad' required='required' type='password' placeholder='ex. 1234'/>
+                                            <input id='senha_cad' name='senha_cad' required='required' type='password' placeholder='Sua senha'/>
+                                        </p>
+                                        <p> 
+                                            <label for='senha_cad'>Insira novamente sua senha</label>
+                                            <input id='senha_cad' name='senha_cad' required='required' type='password' placeholder='Repita sua senha anterior'/>
+                                        </p>
+                                        <p> 
+                                            <label for='senha_cad'>CPF</label>
+                                            <input id='senha_cad' name='senha_cad' required='required' type='number' placeholder='Insira seu CPF'/>
+                                        </p>
+                                        <p> 
+                                            <label for='senha_cad'>Celular</label>
+                                            <input id='senha_cad' name='senha_cad' required='required' type='number' placeholder='(xx) xxxxxxxxx'/>
+                                        </p>
+                                        <p> 
+                                            <label for='senha_cad'>Endereço </label>
+                                            <input id='senha_cad' name='senha_cad' required='required' type='text' placeholder='Rua ou Avenida'/>
+                                        </p>
+                                        <p> 
+                                            <label for='senha_cad'>CEP </label>
+                                            <input id='senha_cad' name='senha_cad' required='required' type='number' placeholder='xxxxxxx'/>
+                                        </p>
+                                        <p> 
+                                            <label for='senha_cad'>Número </label>
+                                            <input id='senha_cad' name='senha_cad' required='required' type='number' placeholder='Número'/>
                                         </p>
                                         
                                         <p> 
@@ -84,7 +106,7 @@ const PageLogin = props => {
                 </div>
 
             </div>
-            <Footer />
+  
         </div>
     );
 
